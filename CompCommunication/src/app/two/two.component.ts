a27230ac1a3342be47bceacdaf38056e2ceb6666
Pavan Kumar <pavan.purohit16@gmail.com>
2021-12-18 16:58:39 -0500

    component communication - @ViewChildren examples

--- CompCommunication/src/app/two/two.component.ts
+++ CompCommunication/src/app/two/two.component.ts
@@ -19,8 +19,10 @@ export class TwoComponent {
 
   @ViewChildren(OneComponent)
   private _map: QueryList<OneComponent> = new QueryList();
-  private _myArr: Array<any>;
+  private _myArr: Array<any>; //for iterations
 
+  //whenever my View is getting initiated and whatever the data we are having in my map function and just converting that into Array.
+  //Only after my View is getting Initialized, whatever the data we are having in this map (_map) function.. _map It contains all the component differences and I am trying to copy that into Array ( _myArr)..  _myArr will contains iteration process.. but QueryList is not having any kind of iteration so because of that I am using.
   ngAfterViewInit(): void {
     this._myArr = this._map.toArray();
   }
@@ -28,10 +30,13 @@ export class TwoComponent {
   ClickMe() {
     //this._obj.var_one = "Welcome_1";
     //this._obj.var_two = "Welcome_2";
-    this._myArr.forEach((el) => {
-      el.var_one = "Welcome_1";
-      el.var_two = "Welcome_2";
-    });
+    //forEach conatins element (el) reference..
+    this._myArr.forEach(
+      (el) => {
+        el.var_one = "Welcome_1";  //If its "var_one" changing element to "Welcome_1
+        el.var_two = "Welcome_2";
+      }
+    );
   }
 
 }
